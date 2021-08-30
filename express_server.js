7f3812148cea3f69a9aa0f62aacb18bf1dd69c8c
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-30 18:13:18 -0400

    Created a  partials directory and included the header partials inside of index and urls_show

--- express_server.js
+++ express_server.js
@@ -24,6 +24,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL-:longURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL:req.params.longURL};
+  res.render("urls_show", templateVars);
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
