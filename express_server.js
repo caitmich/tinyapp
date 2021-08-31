806a536c8c0678eb3090b48148f20abe395a47d7
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-30 21:10:39 -0400

    included body parser and app.post to handle the user input

--- express_server.js
+++ express_server.js
@@ -4,15 +4,28 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok'
+});
+
 // object used to keep track of all urls and their short-forms:
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function generateRandomString() {
+
+};
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -24,14 +37,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: (urlDatabase[req.params.shortURL])};
   res.render("urls_show", templateVars);
 })
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-})
 
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
